f4feb165e536c9dd091a704f00e41d9a031368ea
Erfan Saatchi <112129390+accelangel@users.noreply.github.com>
2023-06-18 00:51:23 -0400

    Build basic header/main, sidebar/workspace layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles.css'
+
+const initialize = function () {
+    let container = document.createElement('div');
+    container.id = 'container'; container.classList.add('container');
+
+    let header = document.createElement('div');
+    header.id = 'header'; header.classList.add('header');
+
+    let main = document.createElement('div');
+    main.id = 'main'; main.classList.add('main');
+
+
+
+
+    container.append(header, main);
+    document.body.append(container);
+};
+
+initialize();
